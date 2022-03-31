b9aa3121b9be95d7ea7225b787bdbc93d440027e
Thibaud Trarieux <thibaud.trarieux@edu.devinci.fr>
2022-03-31 23:43:59 +0200

    change versel link onportfolio.js

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -32,7 +32,7 @@ const setCurrentProducts = ({result, meta}) => {
 const fetchProducts = async (page = 1, size = 12) => {
   try {
     const response = await fetch(
-      `https://clear-fashion-api.vercel.app?page=${page}&size=${size}`
+      `https://clear-fashion-server-gray.vercel.app?page=${page}&size=${size}`
     );
     const body = await response.json();
     var groupbyBrand = body.data.result.reduce(function(groups, item) { //create bybrand options
